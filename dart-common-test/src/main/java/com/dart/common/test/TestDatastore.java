dd8f7b639e364671f2bb5569652467a58079fa1a
Reynald Pader <reynaldmpader@gmail.com>
2015-07-28 18:03:03 +0800

    Add javadoc to TestDatastore

--- dart-common-test/src/main/java/com/dart/common/test/TestDatastore.java
+++ dart-common-test/src/main/java/com/dart/common/test/TestDatastore.java
@@ -5,7 +5,16 @@ import com.google.appengine.tools.development.testing.LocalServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import org.junit.rules.ExternalResource;
 
-
+/**
+ * <p>
+ * Wrapped {@link LocalServiceTestHelper} as JUnit {@link ExternalResource} Rule.
+ * This is to help testers by automatically setting up the local Datastore with
+ * strong consistency before each test case. The Datastore is also torn down after each test.
+ * </p>
+ * <p>
+ * <strong>NOTE:</strong> It may still be necessary to place a Thread.sleep() when testing persist operations.
+ * </p>
+ */
 public class TestDatastore extends ExternalResource {
 
     private LocalServiceTestHelper helper;
