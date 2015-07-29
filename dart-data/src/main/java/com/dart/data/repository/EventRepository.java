62b6f0321eedacdbd29070c111b84cdbf255d5af
Reynald Pader <reynaldmpader@gmail.com>
2015-07-29 09:10:38 +0800

    Change EventRepository interface
    Breaking change. Modified retrieval methods to include search areas. Since the 
    application will be focused on using maps, retrievals should be focused on search
    areas instead of linear time-based query. Existing time-based queries by uer have been
    modified to query based on whether the events have been finished or not. Previously,
    these queries are based upon the time they have been created which may not actually
    be of any use to the client. As of now.

--- dart-data/src/main/java/com/dart/data/repository/EventRepository.java
+++ dart-data/src/main/java/com/dart/data/repository/EventRepository.java
@@ -2,6 +2,8 @@ package com.dart.data.repository;
 
 import com.dart.data.domain.Event;
 import com.dart.data.domain.User;
+import com.dart.data.util.Point;
+import com.dart.data.util.Rectangle;
 
 import java.util.Collection;
 import java.util.Date;
@@ -13,10 +15,45 @@ import java.util.Date;
  */
 public interface EventRepository extends CrudRepository<Event> {
 
-    Collection<Event> findEventsByUserBefore(User organizer, Date date, int limit);
+    /**
+     * Retrieves the {@link Event} entities that are located within the given rectangular area.
+     * The returned entities are those who lie within the search area and have their end dates
+     * before the time of the query.
+     *
+     * @param area the rectangular area - defined by its NE and SW corner points to search in.
+     * @return the collection of unfinished events.
+     */
+    Collection<Event> findUnfinishedEventsArea(Rectangle area);
 
-    Collection<Event> findEventsCreatedBefore(Date date, int limit);
+    /**
+     * Retrieves the {@link Event} entities that are located within the given circle.
+     * The returned entities are those who lie within the search area and have their end dates
+     * before the time of the query.
+     *
+     * @param center the center of the circular area to search in.
+     * @param radius the radius of the circle.
+     * @return the collection of unfinished events.
+     */
+    Collection<Event> findUnfinishedEventsArea(Point center, double radius);
 
-    Collection<Event> findEventsActiveOn(Date date, int limit);
+    /**
+     * Retrieves the {@link Event} entities that have their end dates before the time of the query
+     * and have been organized by the given user.
+     *
+     * @param organizer the center of the circular area to search in.
+     * @return the collection of unfinished events.
+     */
+    Collection<Event> findUnfinishedEventsByUser(User organizer);
+
+    /**
+     * Retrieves the {@link Event} entities that have their end dates after the given time
+     * and have been organized by the given user.
+     *
+     * @param organizer the center of the circular area to search in.
+     * @param date      them time when the events should have ended.
+     * @param limit     the maximum number of events that will be returned.
+     * @return the collection of unfinished events.
+     */
+    Collection<Event> findFinishedEventsByUser(User organizer, Date date, int limit);
 
 }
