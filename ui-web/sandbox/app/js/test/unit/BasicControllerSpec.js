7c8f25515d79203b083c4b6d8c248b0fd7d6904f
Reynald Pader <reynaldmpader@gmail.com>
2015-07-31 14:17:06 +0800

    Update unit test with injections
    The controller will be invoked with $routeParams.
    Tried doing assertions on value assignments from
    the injected value. It werked. huehuehue

--- ui-web/sandbox/app/js/test/unit/BasicControllerSpec.js
+++ ui-web/sandbox/app/js/test/unit/BasicControllerSpec.js
@@ -2,22 +2,13 @@
 define([ "basic/module" ], function(notepad) {
   describe('Basic Controller', function() {
     
-    beforeAll(function() {
-      angular.bootstrap(document, [ 'basicModule' ]);
-    });
-    
     beforeEach(module('basicModule'));
 
-    var $controller;
-
-    beforeEach(inject(function(_$controller_) {
-      // The injector unwraps the underscores (_) from around the parameter
-      // names when matching
-      $controller = _$controller_;
-    }));
     it('should define basic controller', inject(function($controller) {
-      var BasicCtrl = $controller('BasicCtrl');
+      var $routeParams = {name:'strong'};
+      var BasicCtrl = $controller('BasicCtrl',{$routeParams:$routeParams});
       expect(BasicCtrl).toBeDefined();
+      expect(BasicCtrl.ideaName).toEqual('strong');
     }));
   });
 });
