b0f06aef8d4880a3dcaa4dc864420b2bb581ac6b
Reynald Pader <reynaldmpader@gmail.com>
2015-08-12 15:32:25 +0800

    Implement a class that will abstract the way properties are provided

---
+++ dart-common-service/src/main/java/com/dart/common/service/properties/PropertiesProvider.java
@@ -0,0 +1,26 @@
+package com.dart.common.service.properties;
+
+/**
+ * @author RMPader
+ */
+public interface PropertiesProvider {
+
+    //GAE Properties
+    String getGoogleAppId();
+    String getGoogleAppSecret();
+
+    //Login Properties
+    String getGplusAppId();
+    String getGplusAppSecret();
+    String getGplusEndpoint();
+
+    String getFacebookAppId();
+    String getFacebookSecret();
+    String getFacebookEndpoint();
+
+    //Other stuff
+    String getUserWebsiteURL();
+    String getOtherSecret();
+    String getSignupEmailTemplate();
+    String getAppName();
+}
