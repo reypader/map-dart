2ba9b5b570d3e5f705e593952800af1f2928bb85
Reynald Pader <reynaldmpader@gmail.com>
2015-09-14 10:48:16 +0800

    Use DigestUtils Apache Commons Codec Library
    Digest Utils from Google Repacked libraries is deprecated

--- dart-data-objectify/src/main/java/com/dart/data/objectify/factory/UserFactoryImpl.java
+++ dart-data-objectify/src/main/java/com/dart/data/objectify/factory/UserFactoryImpl.java
@@ -3,7 +3,7 @@ package com.dart.data.objectify.factory;
 import com.dart.data.domain.User;
 import com.dart.data.factory.UserFactory;
 import com.dart.data.objectify.domain.UserImpl;
-import com.google.appengine.repackaged.org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.codec.digest.DigestUtils;
 
 import java.util.UUID;
 
@@ -14,7 +14,7 @@ public class UserFactoryImpl implements UserFactory {
 
     @Override
     public User createUser(String email, String displayName) {
-        String secret = DigestUtils.shaHex(UUID.randomUUID().toString());
+        String secret = DigestUtils.sha256Hex(UUID.randomUUID().toString());
         User instance = new UserImpl(email, displayName);
         instance.setSecret(secret);
         return instance;
